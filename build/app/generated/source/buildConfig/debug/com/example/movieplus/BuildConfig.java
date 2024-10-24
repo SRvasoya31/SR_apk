13f8a918c42d59d7d361cff153e7babc332ab82f
SRvasoya <srvasoya5@gmai.com>
2024-10-24 20:48:45 +0530

    first pro

---
+++ build/app/generated/source/buildConfig/debug/com/example/movieplus/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.movieplus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.movieplus";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
